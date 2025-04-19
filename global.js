f682e5bf7d9dfdcf0b8a8d669f808300f9fcb6d7
eshamir3 <156357368+eshamir3@users.noreply.github.com>
2025-04-18 23:13:28 -0700

    nav

--- global.js
+++ global.js
@@ -3,3 +3,13 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the 'current' class if the link exists
+currentLink?.classList.add("current");
