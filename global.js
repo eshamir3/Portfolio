5e1a9a5fb63e8f71053011d55ab22b0571ad5c6c
eshamir3 <156357368+eshamir3@users.noreply.github.com>
2025-04-26 00:16:06 -0700

    steps 1-3

--- global.js
+++ global.js
@@ -100,4 +100,19 @@ form?.addEventListener("submit", (event) => {
   location.href = url; // opens default mail app with prefilled subject/body
 });
 
+
+export async function fetchJSON(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      console.log(response); // Optional: For debugging
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+  
   
