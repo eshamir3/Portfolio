f5d5e3ae14fcb6453175e3bebe64411156ed7790
eshamir3 <156357368+eshamir3@users.noreply.github.com>
2025-05-02 13:34:40 -0700

    proj

--- projects/projects.js
+++ projects/projects.js
@@ -4,4 +4,18 @@ const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
-renderProjects(projects, projectsContainer, 'h2');
+function renderProject(project) {
+    const article = document.createElement("article");
+    article.className = "project";
+  
+    article.innerHTML = `
+      <h2>${project.title}</h2>
+      <div class="project-text">
+        <p>${project.description}</p>
+        <p class="project-year">c. ${project.year}</p>
+      </div>
+    `;
+  
+    return article;
+  }
+  
