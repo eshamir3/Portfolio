cf4b3446f550576c73e59a6be60fa9a6d96e23ff
eshamir3 <156357368+eshamir3@users.noreply.github.com>
2025-05-09 14:51:51 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -138,71 +138,80 @@ function brushed(event) {
 }
 
 function renderScatterPlot(commits) {
-  const width = 1200;
-  const height = 700;
-  const margin = { top: 20, right: 40, bottom: 50, left: 60 };
-
-  const usableArea = {
-    left: margin.left,
-    top: margin.top,
-    right: width - margin.right,
-    bottom: height - margin.bottom,
-    width: width - margin.left - margin.right,
-    height: height - margin.top - margin.bottom,
-  };
-
-  const svg = d3.select('#chart')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('overflow', 'visible');
-
-  xScale = d3.scaleTime()
-    .domain(d3.extent(commits, d => d.datetime))
-    .range([usableArea.left, usableArea.right])
-    .nice();
-
-  yScale = d3.scaleLinear().domain([0, 24]).range([usableArea.bottom, usableArea.top]);
-
-  svg.append('g')
-    .attr('class', 'gridlines')
-    .attr('transform', `translate(${usableArea.left}, 0)`)
-    .call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
-
-  svg.append('g')
-    .attr('transform', `translate(0, ${usableArea.bottom})`)
-    .call(d3.axisBottom(xScale));
-
-  svg.append('g')
-    .attr('transform', `translate(${usableArea.left}, 0)`)
-    .call(d3.axisLeft(yScale).tickFormat(d => `${String(d).padStart(2, '0')}:00`));
-
-  svg.call(d3.brush().extent([[usableArea.left, usableArea.top], [usableArea.right, usableArea.bottom]]).on('start brush end', brushed));
-
-  const validCommits = commits.filter(d => d.datetime instanceof Date && !isNaN(d.datetime));
-  const [minLines, maxLines] = d3.extent(validCommits, d => d.totalLines);
-  const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 30]);
-
-  const dots = svg.append('g').attr('class', 'dots');
-  dots.selectAll('circle')
-    .data(d3.sort(validCommits, d => -d.totalLines))
-    .join('circle')
-    .attr('cx', d => xScale(d.datetime))
-    .attr('cy', d => yScale(d.hourFrac))
-    .attr('r', d => rScale(d.totalLines))
-    .attr('fill', '#4682b4')
-    .style('fill-opacity', 0.7)
-    .on('mouseenter', (event, commit) => {
-      d3.select(event.currentTarget).style('fill-opacity', 1);
-      renderTooltipContent(commit);
-      updateTooltipVisibility(true);
-      updateTooltipPosition(event);
-    })
-    .on('mouseleave', (event) => {
-      d3.select(event.currentTarget).style('fill-opacity', 0.7);
-      updateTooltipVisibility(false);
-    });
-}
+    const width = 1000;
+    const height = 500;
+    const margin = { top: 30, right: 30, bottom: 50, left: 60 };
+  
+    const usableArea = {
+      left: margin.left,
+      top: margin.top,
+      right: width - margin.right,
+      bottom: height - margin.bottom,
+      width: width - margin.left - margin.right,
+      height: height - margin.top - margin.bottom,
+    };
+  
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .attr('preserveAspectRatio', 'xMidYMid meet')
+      .style('width', '100%')
+      .style('max-width', '1000px') // limit size on large screens
+      .style('height', 'auto');
+  
+    xScale = d3.scaleTime()
+      .domain(d3.extent(commits, d => d.datetime))
+      .range([usableArea.left, usableArea.right])
+      .nice();
+  
+    yScale = d3.scaleLinear()
+      .domain([0, 24])
+      .range([usableArea.bottom, usableArea.top]);
+  
+    svg.append('g')
+      .attr('class', 'gridlines')
+      .attr('transform', `translate(${usableArea.left}, 0)`)
+      .call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+  
+    svg.append('g')
+      .attr('transform', `translate(0, ${usableArea.bottom})`)
+      .call(d3.axisBottom(xScale));
+  
+    svg.append('g')
+      .attr('transform', `translate(${usableArea.left}, 0)`)
+      .call(d3.axisLeft(yScale).tickFormat(d => `${String(d).padStart(2, '0')}:00`));
+  
+    svg.call(
+      d3.brush()
+        .extent([[usableArea.left, usableArea.top], [usableArea.right, usableArea.bottom]])
+        .on('start brush end', brushed)
+    );
+  
+    const validCommits = commits.filter(d => d.datetime instanceof Date && !isNaN(d.datetime));
+    const [minLines, maxLines] = d3.extent(validCommits, d => d.totalLines);
+    const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 25]);
+  
+    const dots = svg.append('g').attr('class', 'dots');
+    dots.selectAll('circle')
+      .data(d3.sort(validCommits, d => -d.totalLines))
+      .join('circle')
+      .attr('cx', d => xScale(d.datetime))
+      .attr('cy', d => yScale(d.hourFrac))
+      .attr('r', d => rScale(d.totalLines))
+      .attr('fill', '#4682b4')
+      .style('fill-opacity', 0.7)
+      .on('mouseenter', (event, commit) => {
+        d3.select(event.currentTarget).style('fill-opacity', 1);
+        renderTooltipContent(commit);
+        updateTooltipVisibility(true);
+        updateTooltipPosition(event);
+      })
+      .on('mouseleave', (event) => {
+        d3.select(event.currentTarget).style('fill-opacity', 0.7);
+        updateTooltipVisibility(false);
+      });
+  }
+  
 
 // MAIN
 const rawData = await loadData();
